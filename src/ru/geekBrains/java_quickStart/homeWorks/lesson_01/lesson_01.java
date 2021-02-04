1a93150c6c70b4ef7b8e94d3b7608c31b77ca710
Mikhail Kosyakov <kosjak@hotmail.com>
2021-02-04 11:20:44 +0300

    Commit Lesson_01

---
+++ src/ru/geekBrains/java_quickStart/homeWorks/lesson_01/lesson_01.java
@@ -0,0 +1,7 @@
+package ru.geekBrains.java_quickStart.homeWorks.lesson_01;
+
+public class lesson_01 {
+    public static void main(String[] args) {
+        System.out.println("I am learning Java");
+    }
+}
